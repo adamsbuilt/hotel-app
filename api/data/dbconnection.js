f289aca0c473477a5e7e44d8a557b2c369c9e300
Austin Adams <austin.atoms@gmail.com>
2017-07-17 09:39:36 -0500

    mongodb connection

---
+++ api/data/dbconnection.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+var dburl = 'mongodb://localhost:27017/meanhotel';
+
+var _connection = null;
+
+var open = function() {
+  MongoClient.connect(dburl, function(err, db) {
+    if (err) {
+      console.log("Database connection failed");
+      return;
+    }
+    _connection = db;
+    console.log("Database connection open", db);
+  });
+};
+
+var get = function() {
+  return _connection
+};
+
+module.exports = {
+  open : open,
+  get : get
+};
