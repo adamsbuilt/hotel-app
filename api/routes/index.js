a1fdbaec8aa268545354a36622207def8eba2ef5
Austin Adams <austin.atoms@gmail.com>
2017-07-17 09:37:38 -0500

    routing

---
+++ api/routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const ctrlHotels = require('../controllers/hotels_controller.js');
+const ctrlReviews = require('../controllers/reviews_controller.js');
+const ctrlUsers = require('../controllers/users_controller.js')
+
+//Hotel routes
+router
+  .route('/hotels')
+  .get(ctrlHotels.hotelsGetAll)
+  .post(ctrlHotels.hotelsAddOne);
+
+router
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne)
+  .put(ctrlHotels.hotelsUpdateOne)
+  .delete(ctrlHotels.hotelsDeleteOne);
+
+//Review routes
+router
+  .route('/hotels/:hotelId/reviews')
+  .get(ctrlReviews.reviewsGetAll)
+  .post(ctrlUsers.authenticate, ctrlReviews.reviewsAddOne);
+
+router
+  .route('/hotels/:hotelId/reviews/:reviewId')
+  .get(ctrlReviews.reviewsGetOne)
+  .put(ctrlReviews.reviewsUpdateOne)
+  .delete(ctrlReviews.reviewsDeleteOne);
+
+//Authentication
+router
+  .route('/users/register')
+  .post(ctrlUsers.register);
+
+router
+  .route('/users/login')
+  .post(ctrlUsers.login)
+
+module.exports = router;
