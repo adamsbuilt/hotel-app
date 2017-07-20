e401649c4093f37d3e48b90ff5e7424731686fb6
Austin Adams <austin.atoms@gmail.com>
2017-07-19 19:05:30 -0500

    add one

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -135,4 +135,45 @@ module.exports.hotelsGetOne = function (req, res) {
     });
 
 };
- 
+
+const _splitArray = function(input) {
+  var output;
+  if (input && input.length > 0) {
+    output = input.split(";");
+  } else {
+    output = [];
+  }
+  return output;
+};
+
+module.exports.hotelsAddOne = function (req, res) {
+  Hotel
+  .create({
+    name : req.body.name,
+    description : req.body.description,
+    stars : parseInt(req.body.stars, 10),
+    services : _splitArray(req.body.services),
+    photos : _splitArray(req.body.photos),
+    currency : req.body.currency,
+    location : {
+      address : req.body.address,
+      coordinates : [
+        parseFloat(req.body.lng),
+        parseFloat (req.body.lat)
+      ]
+    }
+  }, function(err, hotel) {
+    if (err) {
+      console.log("ERROR creating hotel");
+    res
+      .status(400)
+      .json(err)
+    } else {
+      console.log("Hotel Created", hotel);
+      res
+        .status(201)
+        .json(hotel)
+    }
+  });
+
+};
