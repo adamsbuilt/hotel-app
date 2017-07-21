84758408e3f5fb4dddc9377725024fda96a4b6cb
Austin Adams <austin.atoms@gmail.com>
2017-07-21 12:06:47 -0500

    user login

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
 
 module.exports.register = function(req, res) {
   console.log('registering user');
@@ -20,6 +21,29 @@ module.exports.register = function(req, res) {
     } else {
       console.log('User Created', user);
       res.status(201).json(user);
+    }
+  });
+};
+
+module.exports.login = function(req, res) {
+  console.log('logging in user');
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({
+    username: username
+  }).exec(function(err, user) {
+    if (err) {
+      console.log(err);
+      res.status(400).json(err);
+     } else {
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log('User Found', user);
+        var token = jwt.sign({ username: user.username }, 's3cr3t', { expiresIn: 3600});
+        res.status(200).json({success: true, token: token});
+      } else {
+        res.status(401).json('UNAUTHORIZED')
+      }
     }
   });
 };
