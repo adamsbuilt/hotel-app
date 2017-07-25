7bb2b5f5b1f66a4dc922969e1b39bc46ca467d0b
Austin Adams <austin.atoms@gmail.com>
2017-07-24 20:05:36 -0500

    get one review call

--- api/controllers/reviewController.js
+++ api/controllers/reviewController.js
@@ -32,3 +32,45 @@ module.exports.reviewsGetAll = function(req, res) {
         .json(response.message)
     });
 };
+
+//GET single review for a hotel
+module.exports.reviewsGetOne = function(req, res) {
+  var hotelId = req.params.hotelId;
+  var reviewId = req.params.reviewId;
+  console.log("GET reviewId " + reviewId + " for hotelId " + hotelId);
+
+  Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, hotel) {
+      var response = {
+        status : 200,
+        message : {}
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!hotel) {
+        console.log("Hotel id not found in database", hotelId);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + hotelId
+        };
+      } else {
+        //Get the review
+        response.message = hotel.reviews.id(reviewId);
+        // If the review doesn't exist Mongoose returns null
+        if (!response.message) {
+          response.status = 404;
+          response.message = {
+            "message" : "Review ID not found " + reviewId
+          };
+        }
+      }
+      res
+        .status(response.status)
+        .json(response.message);
+    });
+
+};
